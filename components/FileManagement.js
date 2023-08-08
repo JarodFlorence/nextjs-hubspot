381fba570b0c60173045940b464bee4a45ad74ec
Jarod Florence <jarod.a.florence@gmail.com>
2023-08-08 01:13:38 +0000

    implement preview for images and PDFs

--- components/FileManagement.js
+++ components/FileManagement.js
@@ -1,6 +1,7 @@
 import useSWR from 'swr';
 import axios from 'axios';
 import FileUpload from './FileUpload';
+import { Document, Page } from 'react-pdf';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -16,6 +17,23 @@ const FileManagement = () => {
     }
   };
 
+  const renderPreview = (file) => {
+    const fileExtension = file.split('.').pop();
+    const filePath = `/uploads/${file}`;
+
+    if (fileExtension === 'pdf') {
+      return (
+        <Document file={filePath} className="mb-4 w-64">
+          <Page pageNumber={1} width={200} />
+        </Document>
+      );
+    } else if (['jpg', 'jpeg', 'png', 'gif'].includes(fileExtension)) {
+      return <img src={filePath} alt={file} className="mb-4 w-64" />;
+    } else {
+      return null;
+    }
+  };
+
   if (error) return <div>Error loading files</div>;
   if (!data) return <div>Loading...</div>;
 
@@ -25,13 +43,16 @@ const FileManagement = () => {
       <FileUpload />
       <ul className="mt-4">
         {data.files.map((file) => (
-          <li key={file} className="flex justify-between items-center mb-2">
-            <a href={`/uploads/${file}`} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
-              {file}
-            </a>
-            <button onClick={() => deleteFile(file)} className="text-red-600 hover:text-red-800">
-              Delete
-            </button>
+          <li key={file} className="flex flex-col mb-6">
+            {renderPreview(file)}
+            <div className="flex justify-between items-center">
+              <a href={`/uploads/${file}`} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
+                {file}
+              </a>
+              <button onClick={() => deleteFile(file)} className="text-red-600 hover:text-red-800">
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
